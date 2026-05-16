6ada2823f3fc87790e9b25f049b495bac8fcb3bb
Said20222 <konodio32@gmail.com>
2026-05-16 21:41:33 +0500

    Add player movement with UUnity Input System; remove legacy movement with Input Manager

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -1,29 +1,29 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
+[RequireComponent(typeof(Rigidbody2D))]
 public class PlayerController : MonoBehaviour
 {
     [Header("Movement Settings")]
     [SerializeField] private float moveSpeed = 5f;
 
     private Rigidbody2D rb;
-    private Vector2 movementInput;
+    private Vector2 moveInput;
 
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
     }
 
-    private void Update()
-    {
-        // Get input from the player
-        movementInput.x = Input.GetAxisRaw("Horizontal");
-        movementInput.y = Input.GetAxisRaw("Vertical");
-        movementInput.Normalize();
-    }
-
     private void FixedUpdate()
     {
         // Move the player based on input
-        rb.linearVelocity = movementInput * moveSpeed;
+        rb.linearVelocity = moveInput * moveSpeed;
+    }
+
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        // Read the movement input
+        moveInput = context.ReadValue<Vector2>();
     }
 }
